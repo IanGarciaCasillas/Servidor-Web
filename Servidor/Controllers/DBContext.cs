7f7f9253a8b59a20190abd51f4204688909b5d72
Ian Garcia Casillas <a2006617@institutmontilivi.cat>
2023-04-12 18:16:01 +0200

    First Commit

---
+++ Servidor/Controllers/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Servidor.Model;
+
+namespace Servidor.Controllers
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Client> Client { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=testing;user=root");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        
+
+    }
+}
---
+++ Servidor/Model/Client.cs
@@ -0,0 +1,12 @@
+﻿namespace Servidor.Model
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Correu { get;set; }
+        public string Contrasenya { get;set; }
+        public string Nom { get; set; }
+        public string Cognom { get; set; }
+        public string Token { get; set; }
+    }
+}
